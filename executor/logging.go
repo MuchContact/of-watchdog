3a98404c8002cea96c9c7cb9e4068b3ed7f46680
MuchContact <422549477@qq.com>
2020-09-04 07:59:56 +0800

    fix long log bug

--- executor/logging.go
+++ executor/logging.go
@@ -4,21 +4,57 @@ import (
 	"bufio"
 	"io"
 	"log"
+	"time"
 )
 
+var stdLogBufferSize = 64 * 1024
+
+// logHeader creates the log prefix timestamp. This is uses the
+// format 2006/01/02 15:04:05
+var logHeader = func() []byte {
+	return []byte(time.Now().Format("2006/01/02 15:04:05"))
+}
 // bindLoggingPipe spawns a goroutine for passing through logging of the given output pipe.
 func bindLoggingPipe(name string, pipe io.Reader, output io.Writer) {
 	log.Printf("Started logging %s from function.", name)
-
-	scanner := bufio.NewScanner(pipe)
-	logger := log.New(output, log.Prefix(), log.Flags())
-
+	scanner := bufio.NewReaderSize(pipe, stdLogBufferSize)
 	go func() {
-		for scanner.Scan() {
-			logger.Printf("%s: %s", name, scanner.Text())
-		}
-		if err := scanner.Err(); err != nil {
-			log.Printf("Error scanning %s: %s", name, err.Error())
+		continuation := false
+
+		for {
+			line, lineNotFinish, err := scanner.ReadLine()
+			switch {
+			case err == io.EOF:
+				return
+			case err != nil:
+				log.Printf("Error scanning %s: %s", name, err.Error())
+				return
+			}
+
+			if !continuation {
+				// we are not continuing a previous line, so we
+				// start the log line with the log prefix
+				_, _ = output.Write(logHeader())
+			}
+
+			_, _ = output.Write(line)
+			// the line is longer than stdLogBufferSize
+			if lineNotFinish || continuation {
+
+				// we are continuing a long log line,
+				// but it is the end of the actual line,
+				// so put the newline back in
+				if !lineNotFinish {
+					_, _ = output.Write([]byte{'\n'})
+				}
+
+				continuation = lineNotFinish
+				continue
+			}
+
+			// not a prefix or continuation, so we have written a complete line
+			// so put the newline back in
+			_, _ = output.Write([]byte{'\n'})
 		}
 	}()
 }
